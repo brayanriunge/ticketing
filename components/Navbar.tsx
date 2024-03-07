feee1fe2b99db46613f37d5381ad869e1ab7fb38
RiugeBrian <riungebrayandev@gmail.com>
2024-03-08 00:13:34 +0300

    update imports

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import useMediaQuery from "@/app/hooks/useMediaQuery";
+import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
